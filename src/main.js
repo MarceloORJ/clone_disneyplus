06218e2d123cb22f75df5c4a027205fe4712f871
Marcelo Oliveira <marcelo@exemplo.com>
2023-08-28 12:11:57 -0300

    ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 document.addEventListener('DOMContentLoaded', function(){
 
     const buttons = document.querySelectorAll('[data-tab-button]');
-    
 
+    const questions = document.querySelectorAll('[data-faq-question]');
+
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+
+        if(posicaoAtual < alturaHero){
+            ocultaElemtosDoHeader();
+        }
+        else{
+            exibeElementosDoHeader();
+        }
+    })
+    
+    //seção ed atrações
     for (let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -13,7 +29,30 @@ document.addEventListener('DOMContentLoaded', function(){
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    //seção FAQ, accordion
+    for(let i = 0; i < questions.length; i++){
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
 })
+
+function exibeElementosDoHeader(){
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+function ocultaElemtosDoHeader(){
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo(){
     const buttons = document.querySelectorAll('[data-tab-button]');
